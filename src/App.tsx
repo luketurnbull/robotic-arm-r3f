e2b5ef602a5130dd0b1e00578d78a0d59bb6fc2f
Luke Turnbull <luke.turn.dev@gmail.com>
2025-07-05 17:07:38 +1000

    Removed presentation controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Environment,
   ContactShadows,
   OrbitControls,
-  PresentationControls,
   Stage,
 } from "@react-three/drei";
 import { Model } from "./components/Model";
@@ -15,7 +13,7 @@ function App() {
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas
         shadows
-        camera={{ position: [0, 0, 4], fov: 50 }}
+        camera={{ position: [10, 10, 10], fov: 50 }}
         gl={{ preserveDrawingBuffer: true }}
       >
         <color attach="background" args={["#f0f0f0"]} />
@@ -34,21 +32,13 @@ function App() {
         />
         <pointLight position={[-10, -10, -10]} intensity={0.5} />
 
-        {/* Presentation controls for better model viewing */}
-        <PresentationControls
-          global
-          rotation={[0.13, 0.1, 0]}
-          polar={[-0.4, 0.2]}
-          azimuth={[-1, 0.75]}
-        >
-          <Stage environment="city" intensity={0.6} shadows>
-            <Model scale={0.1} />
-          </Stage>
-        </PresentationControls>
+        <Stage environment="city" intensity={0.6} shadows>
+          <Model scale={0.1} position={[0, 0, 0]} />
+        </Stage>
 
         {/* Contact shadows for realistic ground contact */}
         <ContactShadows
-          position={[0, -1.4, 0]}
+          position={[0, -0.75, 0]}
           opacity={0.75}
           scale={10}
           blur={2.5}
@@ -56,13 +46,7 @@ function App() {
         />
 
         {/* Orbit controls for camera manipulation */}
-        <OrbitControls
-          enablePan={true}
-          enableZoom={true}
-          enableRotate={true}
-          minDistance={2}
-          maxDistance={10}
-        />
+        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
       </Canvas>
     </div>
   );
